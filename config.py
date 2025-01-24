0cbbf5ccb2058ef74762c7a56e2bc8d8642b2fbd
Gerni <mgerni3421@gmail.com>
2025-01-24 16:42:18 +0000

    create .pem file for database from env var

--- config.py
+++ config.py
@@ -1,5 +1,12 @@
 import os
 import pymongo
+import base64
+
+def create_pem_from_base64(base64_string, filename):
+    decoded_data = base64.b64decode(base64_string)
+
+    with open(filename, "wb") as pem_file:
+        pem_file.write(decoded_data)
 
 IS_PROD = os.environ["FLASK_ENV"].lower() != "development"
 SECRET_KEY = os.environ["SECRET_KEY"]
@@ -7,9 +14,10 @@ SENDGRID_API_KEY = os.environ["SENDGRID_API_KEY"]
 MONGO_URL = os.environ["MONGO_URI"]
 
 if IS_PROD:
-    X509_CERT = os.environ["X509_CERT"]
+    X509_CERT = os.environ["X509"]
+    create_pem_from_base64(X509_CERT, "certificate.pem")
     MONGO_CLIENT = pymongo.MongoClient(MONGO_URL,
                                        tls=True,
-                                       tlsCertificateKeyFile=X509_CERT)
+                                       tlsCertificateKeyFile="certificate.pem")
 else:
     MONGO_CLIENT = pymongo.MongoClient(MONGO_URL)
