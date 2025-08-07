6d3128d155f1b6a0e636b5a1b60b5a7c1a7b9ccc
Gerni <47844854+mgerni@users.noreply.github.com>
2025-08-06 21:12:46 -0400

    fix-lms-task-progress

--- user_dao.py
+++ user_dao.py
@@ -3,7 +3,7 @@ import tasklists
 from dataclasses import dataclass
 from bson.objectid import ObjectId
 from task_types import UserTaskList, TierProgress, UserCompletedTask, UserCurrentTask, TaskData, PageTask
-
+from uuid import UUID
 
 @dataclass
 class UserDatabaseObject:
@@ -69,8 +69,43 @@ class UserDatabaseObject:
         def clean_tasklists(tier: str):
             tasklist = self.get_task_list(tier)
             tasklist.completed_tasks = list({task.uuid: task for task in tasklist.completed_tasks}.values())
+            if not self.lms_enabled:
+                lms_uuids = {
+                            "df3f714e-eb7e-4b86-8b73-f25d6ebc3020",
+                            "bf07a401-9a81-4520-9dd8-1c5af2bc5986",
+                            "69b50b44-33f2-485e-a4e0-195e8f8fa044",
+                            "60cbbdb2-b233-48a0-8413-8867217ce53a",
+                            "4ba185cd-ee9b-45ce-b2c9-10462a8ee843",
+                            "0e7ab87d-cde1-4224-b731-0f4b16308e59",
+                            "2cff2acd-a0c8-446b-a83b-9ac734b308ad",
+                            "bc76ea57-5ef9-4ab0-bdcb-37c410c2183a",
+                            "1cf06859-ff55-476e-9387-80ca82f480ca",
+                            "5407a890-c3dd-4453-96a2-59f97fc98479",
+                            "11f65391-8e33-49dc-bd81-47744eb34ed9",
+                            "2a5428a2-a6a5-44fd-ad89-d8bb4d1cf516",
+                            "116ba1f6-6416-4f8f-8e32-42f096b3fe04",
+                            "ccf42f4a-78e9-4968-b696-b75522198ef4",
+                            "fa5841dd-eb4f-4140-84aa-e10201e3f1cb",
+                            "e218b954-430c-4baf-b2eb-aa174cddbfc7",
+                            "655a77fa-21f6-43e8-9cde-5eb2b660f829",
+                            "347d5712-ebc3-4b74-8aba-ba909c39013e",
+                            "2eb6623e-a326-4b63-9cae-8fa0823c5d49",
+                            "50b61b0e-70d0-40a9-bf42-c1b8ac917717",
+                            "917b912c-2716-4b12-a020-a758ee467e72",
+                            "5cbcc790-10d1-4c17-8b39-cad7b48dadf8",
+                            "4a387bb0-dfc3-4374-aa35-9bacfc2fc92d"
+                            }
+                lms_uuid_set = {UUID(u) for u in lms_uuids}
+                tasklist.completed_tasks = [
+                    task for task in tasklist.completed_tasks
+                    if UUID(task.uuid) not in lms_uuid_set
+                ]
+            print("-"*100)
+            print(tasklist)
+            print("-"*100)
             return tasklist
         completed = len(clean_tasklists(tier).completed_tasks)
+        
         # completed = len(self.get_task_list(tier).completed_tasks)
         total_tasks = tasklists.list_for_tier(tier, self.lms_enabled)
         total = len(total_tasks)
